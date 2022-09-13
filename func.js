56e7b5d3a3c71f7e9e0583ef5e11d2fa31f0faff
github_username <you@domain.com>
2022-09-13 23:48:16 +0600

    second

--- func.js
+++ func.js
@@ -4,4 +4,23 @@ var dataItem = {
     subject:'Accounting'
 }
 
-console.log(dataItem.class)
+//  no 1 declared name
+
+console.log(dataItem.class);
+
+//  no 2 declared name
+
+var dataitem = dataItem["class"];
+console.log(dataitem);
+
+//  no 3 declared name
+
+var dataItems ="class";
+var data=dataItem[dataItems];
+
+// enumerable
+
+// var propertice = Object.keys(dataItem);
+// console.log(propertice)
+var propertice = Object.entries(dataItem);
+console.log(propertice)
